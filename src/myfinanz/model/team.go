4b9cb3484f8813b10d2010b227c59c71a98e3658
Mikka Jenne <mikka.jenne>
2021-06-22 16:27:06 +0200

    fixed team domain model

--- src/myfinanz/model/team.go
+++ src/myfinanz/model/team.go
@@ -2,9 +2,12 @@ package model
 
 type Team struct {
 	// TeamID     int64
-	NameOfTeam string   `bson:"name_of_team,omitempty"`
-	TeamLeader string   `bson:"team_leader,omitempty"`
-	TeamBudget float64  `bson:"team_budget,omitempty"`
-	Category   Category `bson:"category,omitempty"`
-	Players    []string `bson:"players_id,omitempty"`
+	NameOfTeam        string   `bson:"name_of_team,omitempty"`
+	TeamLeader        string   `bson:"team_leader,omitempty"`
+	TeamBudget        float64  `bson:"team_budget,omitempty"`
+	Category          Category `bson:"category,omitempty"`
+	HumanResources    []string `bson:"human_resources_id,omitempty"`
+	NonHumanResources []string `bson:"non_human_resources_id,omitempty"`
+	// HumanResources    []HumanResources `bson:"human_resources_id,omitempty"`
+	// NonHumanResources []NonHumanResources `bson:"non_human_resources_id,omitempty"`
 }
